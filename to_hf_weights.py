62b3b27b790fa02e1e9f954e158f2b100a0f8de2
Eghbal Rahimikia <eghbal.rahimikia@gmail.com>
2022-02-16 19:01:59 +0000

    Update to_hf_weights.py

--- to_hf_weights.py
+++ to_hf_weights.py
@@ -79,9 +79,7 @@ def process_args(
     config,
     output_path,
     dtype,
-    cpu,
-    **kwargs,
-):
+    cpu):
     # validate paths and turn them into Pathy paths.
     input_ckpt = Pathy.fluid(str(input_ckpt))
     assert input_ckpt.is_dir(), f'no such directory "{input_ckpt}"'
